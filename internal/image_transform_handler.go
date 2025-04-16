be7d3d349af1f7f787d4be787fc0fb3437a23443
Piotr Chmolowski <piotr@chmolowski.pl>
2025-04-16 19:27:08 +0200

    Handle If-None-Match/304 more efficiently

--- internal/image_transform_handler.go
+++ internal/image_transform_handler.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/davidbyttow/govips/v2/vips"
 )
@@ -19,8 +20,34 @@ func NewImageTransformHandler(config *Config) *ImageTransformHandler {
 	return &ImageTransformHandler{config}
 }
 
+func generateETag(sourceURL string, imageOptions *ImageOptions) string {
+	h := sha1.New()
+
+	io.WriteString(h, sourceURL)
+	io.WriteString(h, fmt.Sprintf("%d", imageOptions.Width))
+	io.WriteString(h, fmt.Sprintf("%d", imageOptions.Height))
+	io.WriteString(h, fmt.Sprintf("%d", imageOptions.Quality))
+	io.WriteString(h, fmt.Sprintf("%v", imageOptions.StripMetadata))
+	io.WriteString(h, fmt.Sprintf("%v", imageOptions.Format))
+	io.WriteString(h, strings.Join(imageOptions.Operations, ","))
+
+	return fmt.Sprintf("\"%x\"", h.Sum(nil))
+}
+
 func (h *ImageTransformHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	imageSource := getImageSource(r.Context())
+	imageOptions := NewImageOptionsFromRequest(r)
+
+	etag := generateETag(imageSource.URL, imageOptions)
+	w.Header().Set("ETag", etag)
+
+	if ifNoneMatch := r.Header.Get("If-None-Match"); ifNoneMatch != "" {
+		if strings.Contains(ifNoneMatch, etag) {
+			slog.Info("ETag match", "etag", etag, "ifNoneMatch", ifNoneMatch)
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+	}
 
 	downloadedFile, err := DownloadFile(imageSource.URL, h.config.DownloadMaxSize, h.config.DownloadTimeout)
 	if err != nil {
@@ -36,13 +63,11 @@ func (h *ImageTransformHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	imageOptions := NewImageOptionsFromRequest(r)
 	if imageOptions.Format == vips.ImageTypeUnknown {
 		imageOptions.Format = downloadedImageType
 	}
 
 	processedImage, err := TransformImage(downloadedFile.Buffer.Bytes(), imageOptions)
-
 	if err != nil {
 		slog.Error("TransformImage error", "error", err)
 		http.Error(w, "Error: "+err.Error(), http.StatusInternalServerError)
@@ -54,15 +79,8 @@ func (h *ImageTransformHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	}
 
 	w.Header().Set("Cache-Control", h.config.CacheControl)
-	w.Header().Set("ETag", generateETag(processedImage.Bytes))
 	w.Header().Set("Content-Type", processedImage.Mime)
 	w.Header().Set("Content-Length", strconv.Itoa(processedImage.Size))
 	w.WriteHeader(http.StatusOK)
 	w.Write(processedImage.Bytes)
-}
-
-func generateETag(data []byte) string {
-	h := sha1.New()
-	io.WriteString(h, string(data))
-	return fmt.Sprintf("\"%x\"", h.Sum(nil))
 }
