fd07feb8948155c750dc76883fb3d480f9888f38
Piotr Chmolowski <piotr@chmolowski.pl>
2025-05-06 08:47:11 +0200

    Log URL before signature is removed

--- internal/logging_middleware.go
+++ internal/logging_middleware.go
@@ -18,14 +18,14 @@ func (h *LoggingMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	newWriter := newResponseWriter(w)
 
 	started := time.Now()
+	fullURL := currentRequestHost(r) + r.URL.String()
+
 	h.next.ServeHTTP(newWriter, r)
 	elapsed := time.Since(started)
 
 	userAgent := r.Header.Get("User-Agent")
 	remoteAddr := r.Header.Get("X-Forwarded-For")
 	respContent := newWriter.Header().Get("Content-Type")
-
-	fullURL := currentRequestHost(r) + r.URL.String()
 
 	slog.Info("Request",
 		"method", r.Method,
